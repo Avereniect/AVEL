2f04e5ed1fbfc3ca0d56610dfc0a5cec64a1c651
Avereniect <Avereniect@gmail.com>
2021-12-26 19:06:07 -0800

    Forgot to add these

---
+++ include/avel/impl/Detect_capabilities_Clang.hpp
@@ -0,0 +1,11 @@
+//
+// Created by avereniect on 12/26/21.
+//
+
+#ifndef AVEL_DETECT_CAPABILITIES_GCC_HPP
+#define AVEL_DETECT_CAPABILITIES_GCC_HPP
+
+// Clang just copies GCC's interface
+#include "Detect_capabilities_GCC.hpp"
+
+#endif //AVEL_DETECT_CAPABILITIES_GCC_HPP
---
+++ include/avel/impl/Detect_capabilities_GCC.hpp
@@ -0,0 +1,76 @@
+//
+// Created by avereniect on 12/26/21.
+//
+
+#ifndef AVEL_DETECT_CAPABILITIES_GCC_HPP
+#define AVEL_DETECT_CAPABILITIES_GCC_HPP
+
+#ifdef __SSE__
+    #define AVEL_SSE
+#endif
+
+#ifdef __SSE2__
+    #define AVEL_SSE2
+#endif
+
+#ifdef __SSE3__
+    #define AVEL_SSE3
+#endif
+
+#ifdef __SSSE3__
+    #define AVEL_SSSE3
+#endif
+
+#ifdef __SSE4_1__
+    #define AVEL_SSE41
+#endif
+
+#ifdef __SSE4_1__
+    #define AVEL_SSE41
+#endif
+
+#ifdef __SSE4_2__
+    #define AVEL_SSE41
+#endif
+
+#ifdef __SSE4_2__
+    #define AVEL_SSE41
+#endif
+
+#ifdef __AVX__
+    #define AVEL_AVX
+#endif
+
+#ifdef __AVX2__
+    #define AVEL_AVX2
+#endif
+
+#ifdef __FMA__
+    #define AVEL_FMA
+#endif
+
+#ifdef __AVX512F__
+    #define AVEL_AVX512F
+#endif
+
+#ifdef __AVX512BW__
+    #define AVEL_AVX512BW
+#endif
+
+#ifdef __AVX512VL__
+    #define AVEL_AVX512VL
+#endif
+
+#ifdef __AVX512BITALG__
+    #define AVEL_AVX512_BITALG
+#endif
+
+#ifdef __AVX512VPOPCNTDQ__
+    #define AVEL_AVX512VPOPCNTDQ
+#endif
+
+#ifdef __POPCNT__
+    #define AVEL_POPCNT
+#endif
+
+#endif //AVEL_DETECT_CAPABILITIES_GCC_HPP
