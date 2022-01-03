709eea76d2dd3dc0e05f5a474b71556b63d7bb19
Avereniect <Avereniect@gmail.com>
2022-01-03 15:32:24 -0800

    Initial attempt at vec16x32f sincos

---
+++ include/avel/impl/Utils.hpp
@@ -0,0 +1,31 @@
+//
+// Created by avereniect on 1/3/22.
+//
+
+#ifndef AVEL_UTILS_HPP
+#define AVEL_UTILS_HPP
+
+#include <cstdint>
+#include <type_traits>
+
+namespace avel {
+
+    template<class T, class U>
+    T reinterpret_bits(const U v) {
+        static_assert(sizeof(T) == sizeof(U), "Size of target and source types must be equal");
+        static_assert(std::is_trivial<T>::value, "Target type must be trivial");
+        static_assert(std::is_trivial<U>::value, "Source type must be trivial");
+
+        T ret;
+        auto* src = reinterpret_cast<const unsigned char*>(&v);
+        auto* dst = reinterpret_cast<unsigned char*>(&ret);
+        for (std::size_t i = 0; i < sizeof(T); ++i) {
+            dst[i] = src[i];
+        }
+
+        return ret;
+    }
+
+}
+
+#endif //AVEL_UTILS_HPP
--- include/avel/impl/vectors/Vec16x32f.hpp
+++ include/avel/impl/vectors/Vec16x32f.hpp
@@ -22,10 +22,10 @@ namespace avel {
         //=================================================
 
         AVEL_FINL explicit Vector_mask(const primitive contents):
-            contents(contents) {}
+            content(contents) {}
 
         AVEL_FINL explicit Vector_mask(bool x):
-            contents(from_bool(x)) {}
+            content(from_bool(x)) {}
 
         Vector_mask() = default;
         Vector_mask(const Vector_mask&) = default;
@@ -37,7 +37,7 @@ namespace avel {
         //=================================================
 
         AVEL_FINL Vector_mask& operator=(bool x) {
-            contents = from_bool(x);
+            content = from_bool(x);
             return *this;
         }
 
@@ -49,17 +49,17 @@ namespace avel {
         //=================================================
 
         AVEL_FINL Vector_mask& operator&=(const Vector_mask& rhs) {
-            contents = _kand_mask16(contents, rhs.contents);
+            content = _kand_mask16(content, rhs.content);
             return *this;
         }
 
         AVEL_FINL Vector_mask& operator|=(const Vector_mask& rhs) {
-            contents = _kor_mask16(contents, rhs.contents);
+            content = _kor_mask16(content, rhs.content);
             return *this;
         }
 
         AVEL_FINL Vector_mask& operator^=(const Vector_mask& rhs) {
-            contents = _kxor_mask16(contents, rhs.contents);
+            content = _kxor_mask16(content, rhs.content);
             return *this;
         }
 
@@ -68,28 +68,36 @@ namespace avel {
         //=================================================
 
         AVEL_FINL Vector_mask operator~() const {
-            return Vector_mask{_knot_mask16(contents)};
+            return Vector_mask{_knot_mask16(content)};
         }
 
         AVEL_FINL Vector_mask operator&(Vector_mask rhs) const {
-            return Vector_mask{_kand_mask16(contents, rhs.contents)};
+            return Vector_mask{_kand_mask16(content, rhs.content)};
         }
 
         AVEL_FINL Vector_mask operator|(Vector_mask rhs) const {
-            return Vector_mask{_kor_mask16(contents, rhs.contents)};
+            return Vector_mask{_kor_mask16(content, rhs.content)};
         }
 
         AVEL_FINL Vector_mask operator^(Vector_mask rhs) const {
-            return Vector_mask{_kxor_mask16(contents, rhs.contents)};
+            return Vector_mask{_kxor_mask16(content, rhs.content)};
         }
 
         //=================================================
-        // Accessor
+        // Accessors
         //=================================================
 
         AVEL_FINL bool operator[](int i) const {
-            int m = _mm512_mask2int(contents);
+            int m = _mm512_mask2int(content);
             return m & (1 << i);
+        }
+
+        //=================================================
+        // Conversion operators
+        //=================================================
+
+        AVEL_FINL operator primitive() const {
+            return content;
         }
 
     private:
@@ -98,7 +106,7 @@ namespace avel {
         // Instance members
         //=================================================
 
-        primitive contents;
+        primitive content;
 
         //=================================================
         // Helper functions
@@ -271,23 +279,25 @@ namespace avel {
         // Arithmetic operators
         //=================================================
 
-        AVEL_FINL Vector operator+(Vector vec) {
+        AVEL_FINL Vector operator+(Vector vec) const {
             return Vector{_mm512_add_ps(content, vec.content)};
         }
 
-        AVEL_FINL Vector operator-(Vector vec) {
+        AVEL_FINL Vector operator-(Vector vec) const {
             return Vector{_mm512_sub_ps(content, vec.content)};
         }
 
-        AVEL_FINL Vector operator*(Vector vec) {
+        AVEL_FINL Vector operator*(Vector vec) const {
             return Vector{_mm512_mul_ps(content, vec.content)};
         }
 
-        AVEL_FINL Vector operator/(Vector vec) {
+        AVEL_FINL Vector operator/(Vector vec) const {
             return Vector{_mm512_div_ps(content, vec.content)};
         }
 
-        AVEL_FINL Vector operator%(Vector vec);
+        AVEL_FINL Vector operator%(Vector vec) const {
+            return Vector{_mm512_sub_ps(content, _mm512_mul_ps(trunc(*this / vec), vec))};
+        }
 
         //=================================================
         // Increment/Decrement operators
@@ -366,11 +376,11 @@ namespace avel {
             return array;
         }
 
-        AVEL_FINL operator primitive() {
+        AVEL_FINL operator primitive() const {
             return content;
         }
 
-        AVEL_FINL explicit operator mask() {
+        AVEL_FINL explicit operator mask() const {
             return *this == zeros();
         }
 
@@ -387,6 +397,10 @@ namespace avel {
         primitive content;
 
     };
+
+    AVEL_FINL vec16x32f blend(vec16x32f a, vec16x32f b, vec16x32f::mask m) {
+        return vec16x32f{_mm512_mask_blend_ps(m, a, b)};
+    }
 
     AVEL_FINL vec16x32f frexp(vec16x32f x, vec16x32f* y); //TODO: Implement
 
@@ -413,6 +427,144 @@ namespace avel {
 
     AVEL_FINL vec16x32f sqrt(vec16x32f v) {
         return vec16x32f{_mm512_sqrt_ps(v)};
+    }
+
+    AVEL_FINL vec16x32f fmadd(vec16x32f m, vec16x32f x, vec16x32f b) {
+        return vec16x32f{_mm512_fmadd_ps(m, x, b)};
+    }
+
+    AVEL_FINL vec16x32f fmsub(vec16x32f m, vec16x32f x, vec16x32f b) {
+        return vec16x32f{_mm512_fmsub_ps(m, x, b)};
+    }
+
+    AVEL_FINL vec16x32f fnmadd(vec16x32f m, vec16x32f x, vec16x32f b) {
+        return vec16x32f{_mm512_fnmadd_ps(m, x, b)};
+    }
+
+    AVEL_FINL vec16x32f fnmsub(vec16x32f m, vec16x32f x, vec16x32f b) {
+        return vec16x32f{_mm512_fnmsub_ps(m, x, b)};
+    }
+
+    AVEL_FINL vec16x32f abs(vec16x32f v) {
+        return vec16x32f{_mm512_abs_ps(v)};
+    }
+
+    AVEL_FINL vec16x32f sin(vec16x32f angle) {
+        return vec16x32f{};
+    }
+
+    AVEL_FINL std::array<vec16x32f, 2> sincos(vec16x32f angle) {
+        alignas(32) static const float constants0[8] {
+            reinterpret_bits<float>(std::uint32_t(0x7FFFFFFF)),
+            0.5f,
+            6.283185307179586f,
+            1.0f / (6.283185307179586f),
+            0.78539816339744830f,
+            1.57079632679489661f,
+            1.0f,
+            0.0f
+        };
+
+        __m512 abs_angle = _mm512_abs_ps(angle);
+
+        //Mods input angle to [-pi, pi) range
+        const __m512 half       = _mm512_set1_ps(constants0[0x01]);
+        const __m512 two_pi     = _mm512_set1_ps(constants0[0x02]);
+        const __m512 rcp_two_pi = _mm512_set1_ps(constants0[0x03]);
+        __m512 a = _mm512_fnmadd_ps(two_pi, _mm512_roundscale_ps(_mm512_fmadd_ps(abs_angle, rcp_two_pi, half), _MM_FROUND_TO_ZERO | _MM_FROUND_NO_EXC), abs_angle);
+
+        // Taylor series approximation will always return positive sign
+        __m512 abs_a = _mm512_abs_ps(a);
+
+        __m512 quart_pi = _mm512_set1_ps(constants0[0x04]);
+        __m512 half_pi  = _mm512_set1_ps(constants0[0x05]);
+         __m512 three_quart_pi = _mm512_add_ps(half_pi, quart_pi);
+
+        // Contain the sign of the final return value.
+        __mmask16 c_signs = _mm512_cmp_ps_mask(half_pi, abs_a, _CMP_LT_OQ);
+        __mmask16 s_signs = _kxor_mask16(
+            _mm512_cmp_ps_mask(a, _mm512_setzero_ps(), _CMP_LT_OQ),
+            _mm512_cmp_ps_mask(angle, _mm512_setzero_ps(), _CMP_LT_OQ)
+        );
+
+        // Swap results of Taylor series later if false
+        // Checks to see if angle is more than 45 degrees off the x-axis
+        auto approx_mask = _mm512_cmp_ps_mask(
+            _mm512_sub_ps(_mm512_abs_ps(_mm512_sub_ps(abs_a, half_pi)), quart_pi),
+            _mm512_setzero_ps(),
+            _CMP_GT_OQ
+        );
+
+        // Compute offset that's added to angle to bring it into [-pi/4, pi/4]
+        // range where error of Taylor series is minimized
+        auto add_half0 = _mm512_cmp_ps_mask(quart_pi, abs_a, _CMP_LT_OQ);
+        auto add_half1 = _mm512_cmp_ps_mask(three_quart_pi, abs_a, _CMP_LT_OQ);
+
+        __m512 angle_offset = _mm512_setzero_ps();
+        angle_offset = _mm512_mask_add_ps(angle_offset, add_half0, angle_offset, half_pi);
+        angle_offset = _mm512_mask_add_ps(angle_offset, add_half1, angle_offset, half_pi);
+
+        // Compute exponentiations of adjusted angle
+        __m512 x = _mm512_sub_ps(abs_a, angle_offset);
+        __m512 xx = _mm512_mul_ps(x, x);
+        __m512 xxxx = _mm512_mul_ps(xx, xx);
+        __m512 xxxxxx = _mm512_mul_ps(xx, xxxx);
+        __m512 xxxxxxxx = _mm512_mul_ps(xxxx, xxxx);
+
+        alignas(32) static constexpr float constants1[8] {
+            -1.0f / 2.0f,
+            -1.0f / 6.0f,
+            +1.0f / 24.0f,
+            +1.0f / 120.0f,
+            -1.0f / 720.0f,
+            -1.0f / 5040.0f,
+            +1.0f / 40320.0f,
+            +1.0f / 362880.0f
+        };
+
+        // Denominators for sine terms
+        const __m512 sd0 = _mm512_set1_ps(constants1[0x01]);
+        const __m512 sd1 = _mm512_set1_ps(constants1[0x03]);
+        const __m512 sd2 = _mm512_set1_ps(constants1[0x05]);
+        const __m512 sd3 = _mm512_set1_ps(constants1[0x07]);
+
+        // Sum sine terms
+        __m512 st1 = _mm512_set1_ps(constants0[0x06]);
+        __m512 st2 = _mm512_fmadd_ps(sd0, xx, st1);
+        __m512 st3 = _mm512_fmadd_ps(sd1, xxxx, st2);
+        __m512 st4 = _mm512_fmadd_ps(sd2, xxxxxx, st3);
+        __m512 st5 = _mm512_fmadd_ps(sd3, xxxxxxxx, st4);
+
+        __m512 sin = _mm512_mul_ps(x, st5);
+
+        // Denominators for cosine terms
+        const __m512 cd0 = _mm512_set1_ps(constants1[0x00]);
+        const __m512 cd1 = _mm512_set1_ps(constants1[0x02]);
+        const __m512 cd2 = _mm512_set1_ps(constants1[0x04]);
+        const __m512 cd3 = _mm512_set1_ps(constants1[0x06]);
+
+        // Sum cosine terms
+        __m512 ct1 = _mm512_set1_ps(constants0[0x06]);
+        __m512 ct2 = _mm512_fmadd_ps(cd0, xx, ct1);
+        __m512 ct3 = _mm512_fmadd_ps(cd1, xxxx, ct2);
+        __m512 ct4 = _mm512_fmadd_ps(cd2, xxxxxx, ct3);
+        __m512 ct5 = _mm512_fmadd_ps(cd3, xxxxxxxx, ct4);
+
+        __m512 cos = ct5;
+
+        //Swap bits depending on approx_mask
+        __m512 ret_sin = _mm512_mask_blend_ps(approx_mask, cos, sin);
+        __m512 ret_cos = _mm512_mask_blend_ps(approx_mask, sin, cos);
+
+        // Set sine sign bits
+        ret_sin = _mm512_abs_ps(ret_sin);
+        ret_sin = _mm512_mask_sub_ps(ret_sin, s_signs, _mm512_setzero_ps(), ret_sin);
+
+        // Set cosine sign bits
+        ret_cos = _mm512_abs_ps(ret_cos);
+        ret_cos = _mm512_mask_sub_ps(ret_cos, c_signs, _mm512_setzero_ps(), ret_cos);
+
+        return {vec16x32f{ret_sin}, vec16x32f{ret_cos}};
     }
 
 }
