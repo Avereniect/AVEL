dd67e76fe2891a9039a79fa20cfd16fadcbeb54b
Avereniect <Avereniect@gmail.com>
2022-12-02 10:03:17 -0800

    Added utility function compare_common_bytes

--- include/avel/Misc.hpp
+++ include/avel/Misc.hpp
@@ -34,8 +34,8 @@ namespace avel {
     template<class T, class U>
     AVEL_FINL T bit_cast(const U& u) {
         static_assert(sizeof(T) == sizeof(U), "Size of target and source types must be equal");
-        static_assert(std::is_trivially_copyable<T>::value, "Target type must be trivial");
-        static_assert(std::is_trivially_copyable<U>::value, "Source type must be trivial");
+        static_assert(std::is_trivial<T>::value, "Target type must be trivial");
+        static_assert(std::is_trivial<U>::value, "Source type must be trivial");
 
         #if __cplusplus >= 202002L
         return std::bit_cast<T>(u);
@@ -53,6 +53,27 @@ namespace avel {
 
         return ret;
         #endif
+    }
+
+}
+
+namespace avel_impl {
+
+    template<class T, class U>
+    [[nodiscard]]
+    AVEL_FINL T compare_common_bytes(const T& t, const U& u) {
+        static_assert(std::is_trivial<T>::value, "Target type must be trivial");
+        static_assert(std::is_trivial<U>::value, "Source type must be trivial");
+
+        auto* p0 = reinterpret_cast<const unsigned char*>(&t);
+        auto* p1 = reinterpret_cast<const unsigned char*>(&u);
+
+        bool are_equal = true;
+        for (std::size_t i = 0; i < std::min(sizeof(T), sizeof(U)); ++i) {
+            are_equal &= (p0[i] == p1[i]);
+        }
+
+        return are_equal;
     }
 
 }
