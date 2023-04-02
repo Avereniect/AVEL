670a1975fe7369e3719d908c696b12ffd433f9ce
Avereniect <Avereniect@gmail.com>
2023-04-01 22:50:42 -0700

    Removed incomplete vector implementations

--- include/avel/impl/vectors/Vec16x16u.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by avereniect on 7/30/22.
-//
-
-#ifndef AVEL_VEC16X16_HPP
-#define AVEL_VEC16X16_HPP
-
-#endif //AVEL_VEC16X16_HPP
--- include/avel/impl/vectors/Vec32x16u.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by avereniect on 7/30/22.
-//
-
-#ifndef AVEL_VEC32X16_HPP
-#define AVEL_VEC32X16_HPP
-
-#endif //AVEL_VEC32X16_HPP
